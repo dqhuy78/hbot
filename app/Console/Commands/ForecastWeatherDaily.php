8b72c6dba2a762e2756630abc0a0d2f096b3b0f1
dqhuy78 <dqhuy78@gmail.com>
2020-11-24 20:57:47 +0700

    Update message

--- app/Console/Commands/ForecastWeatherDaily.php
+++ app/Console/Commands/ForecastWeatherDaily.php
@@ -73,7 +73,8 @@ class ForecastWeatherDaily extends Command
         $dayLeftToNewYear2 = (int) round((strtotime('2021-02-12') - time()) / (60 * 60 * 24));
         $date = Carbon::today()->format('d/m/Y');
 
-        return '[info][title]Chào buổi sáng![/title]'
+        return '[toall]'
+            . '[info][title]Chào buổi sáng cả nhà![/title]'
             . '(*) Dự báo thời tiết ngày ' . $date . ': Nhiệt độ: ' . $temperature . ' độ C, ' . $des . PHP_EOL . PHP_EOL
             . '(cracker) Còn ' . $dayLeftToNoel . ' ngày nữa là Noel (https://dqhuy78.github.io/countdown/)' . PHP_EOL
             . '(cracker) Còn ' . $dayLeftToNewYear . ' ngày nữa là tết dương (Comming soon...)' . PHP_EOL
